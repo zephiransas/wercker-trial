4e362c44bae2f681a83cf6ae74c49c7eef226d1d
zephiransas <zephiransas@gmail.com>
2014-01-24 09:22:22 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WerckerTrial::Application.config.secret_key_base = '1a42a01f794d088e1400837c8e327a5764f692a0ebd25d4f41e8379f07d09675ed49f8aad81dbd12793627e1467abd44e2268276837116f06d50c43bc340c7ff'
